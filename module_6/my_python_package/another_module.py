77defbc1f4308b5578346432a91f114b2edc7dac
robalford <robertsalford@gmail.com>
2025-03-05 11:23:39 -0800

    remove requests package code since didn't demo it

--- module_6/my_python_package/another_module.py
+++ module_6/my_python_package/another_module.py
@@ -1,14 +1,6 @@
-# Install Requests library with pip globally to import it into any Python projects, or into a project-specific virtual
-# environment
-# import requests
-
 # Relative imports can be used inside a pacakge
 from .import_me import a_function
-
 
 a_function()
 
 print("I'm another module in the package")
-
-# amazon = requests.get("https://amazon.com")
-# print(amazon.text)
